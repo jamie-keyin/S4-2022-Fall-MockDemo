f8e80f640a7b900608af9cc30dab09de4a45079e
jamie.cornick <jamie.cornick@keyin.com>
2022-10-19 11:04:56 -0230

    Added Airport objects and initial test.

---
+++ src/main/java/com/keyin/airports/Airport.java
@@ -0,0 +1,31 @@
+package com.keyin.airports;
+
+public class Airport {
+    private int id;
+    private String code;
+    private City city;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+}
---
+++ src/main/java/com/keyin/data/Database.java
@@ -0,0 +1,17 @@
+package com.keyin.data;
+
+import com.keyin.airports.Airport;
+import com.keyin.airports.City;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    public City getCityByName(String cityName) {
+        return null;
+    }
+
+    public List<Airport> getAllAirports() {
+        return new ArrayList<Airport>();
+    }
+}
